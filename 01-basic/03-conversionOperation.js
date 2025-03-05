6cbe9444019f308c2b7a4f5147a2dd30c388332a
Meett-patel <i.meetpatel01204@gmail.com>
2025-03-05 12:59:25 +0000

    conversion of data type

---
+++ 01-basic/03-conversionOperation.js
@@ -0,0 +1,23 @@
+// let value = "null"
+// console.log(typeof value);
+
+
+// let valueInNumber = Number(value)
+// console.log(valueInNumber);
+
+// console.log(typeof valueInNumber);
+
+// "33" => 33
+// "33adf" => NaN
+// true => 1; false =>0
+
+let isLoggedIn = 1
+console.log(isLoggedIn);
+console.log(typeof (isLoggedIn));
+
+
+let valueConversion = Boolean(isLoggedIn)
+console.log(valueConversion);
+console.log(typeof (valueConversion));
+
+
